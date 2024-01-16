982f4f394327bbaba442348b1effdc3b5ce7f0e6
rds668 <delgadoshepherdrafael@gmail.com>
2024-01-16 16:59:54 +0100

    Commit Clase 16/01/2024

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,27 +3,3 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
-buildscript{
-    repositories{
-        google()
-        jcenter()
-        maven{"url html://plugins.gradle.org/m2"}
-    }
-
-    dependencies{
-        classpath("com.android.tools.build:gradle:3.5.4")
-        classpath("com.google.gms:google-services:4.2.0")
-        classpath("gradle.plugin.com.onesignal:onesignal-gradle-plugin:0.12.4")
-    }
-}
-
-allprojects{
-    repositories{
-        google()
-        jcenter()
-
-        maven { "url https://jitpack.io" }
-        maven{"url https://maven.google.com"}
-    }
-}
-
