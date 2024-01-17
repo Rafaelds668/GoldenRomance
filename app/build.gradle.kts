390f5bf9a231c8f7b317d972530c413d450fe81e
rds668 <delgadoshepherdrafael@gmail.com>
2024-01-17 09:12:59 +0100

    hola buenas digame su numero de pedido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
@@ -33,12 +36,32 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.3"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
 
     implementation ("fileTree(dir: 'libs', include: ['*.jar'])")
     implementation ("'androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     testImplementation ("junit:junit:4.12")
     androidTestImplementation ("androidx.test:runner:1.3.0")
     androidTestImplementation ("androidx.test.espresso:espresso-core:3.3.0")
@@ -64,7 +87,11 @@ dependencies {
     implementation ("com.github.dimorinny:show-case-card-view:0.0.1")
 
     implementation ("com.github.BakerJQ:Android-InfiniteCards:1.0.5")
-    implementation ("com.google.firebase:firebase-ads:19.7.0")
+    //implementation ("com.google.firebase:firebase-ads:19.7.0")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
 //    implementation 'com.google.android.gms:play-services-ads:19.7.0'
 //
--- app/src/main/java/com/example/goldenromance/SplashScreenActivity.java
+++ app/src/main/java/com/example/goldenromance/SplashScreenActivity.java
@@ -6,18 +6,18 @@ import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this, Choose_Login_And_Reg.class);
+                Intent i = new Intent(SplashScreenActivity.this, Choose_Login_And_Reg.class);
                 startActivity(i);
                 finish();
             }
